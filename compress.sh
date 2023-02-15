7cb85c38a5017c2e5dfeebc949eca7450576e264
Bert Temme <btemme@hotmail.com>
2023-02-15 22:58:25 +0100

    Update compress.sh
    remove batchtable.json

--- compress.sh
+++ compress.sh
@@ -8,8 +8,8 @@ do
   gltf-pipeline -i "$filename".glb -o "$filename".glb -d -b
   b3dm pack -i "$filename".glb -f
   rm "$filename".glb
-  if [ -f "$filename".batch ]; then
-    rm "$filename".batch
+  if [ -f "$filename".batchtable.json ]; then
+    rm "$filename".batchtable.json
   fi
   if [ -f "$filename".featuretable.json ]; then
     rm "$filename".featuretable.json
