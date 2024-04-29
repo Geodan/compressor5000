44811a154af4b140dd0dfa0cecc9cee4d07e35e5
Bert Temme <btemme@hotmail.com>
2024-04-29 15:36:19 +0200

    Update compress.sh

--- compress.sh
+++ compress.sh
@@ -10,7 +10,7 @@ compressB3dm()
   echo "$entry"
   b3dm unpack -i "$entry" -f
   filename="${entry%.*}"
-  gltf-pipeline -i "$filename".glb -o "$filename".glb -d -b
+  gltf-pipeline -i "$filename".glb -o "$filename".glb -d -b --draco.quantizePositionBits 15
   b3dm pack -i "$filename".glb -f
   rm "$filename".glb
   if [ -f "$filename".batchtable.json ]; then
